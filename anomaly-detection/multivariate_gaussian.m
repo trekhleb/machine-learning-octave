a81b16ffd943b6c839d42a88651e69be99d457cc
Oleksii Trekhleb <trehleb@gmail.com>
2018-12-22 10:52:50 +0200

    Fix typo.

--- anomaly-detection/multivariate_gaussian.m
+++ anomaly-detection/multivariate_gaussian.m
@@ -6,7 +6,7 @@ function probabilities = multivariate_gaussian(X, mu, sigma2)
     % Init probabilities matrix.
     probabilities = ones(m, 1);
 
-    % Go through all traing examples and through all features.
+    % Go through all training examples and through all features.
     for i=1:m
         for j=1:n
             p = (1 / sqrt(2 * pi * sigma2(j))) * exp(-(X(i, j) - mu(j)) .^ 2 / (2 * sigma2(j)));
